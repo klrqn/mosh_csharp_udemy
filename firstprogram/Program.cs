6db3bf6632ee6b96b7f57d389d797b8f7df8e026
Scott <burtonsb@gmail.com>
2018-08-20 16:59:55 -0400

    fixed firstapp

---
+++ firstprogram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("again and again");
+        }
+    }
+}
--- notes.cs
+++ notes.cs
@@ -1,14 +1,20 @@
-CLR - compiler so you can compile to different types of computers - Common Language Runtime
+// CLR - compiler so you can compile to different types of computers - Common Language Runtime
 
-C vs .NET - .Net is a framework for writing windows applications
+// C vs .NET - .Net is a framework for writing windows applications
 
-"Namespace" - container for related classes
-Assembly - a container for related namespaces - DLL or EXE
-                                              - Dynamically Linked Library
-Application - container for multiple Assemblies.
+// "Namespace" - container for related classes
+// Assembly - a container for related namespaces - DLL or EXE
+//                                               - Dynamically Linked Library
+// Application - container for multiple Assemblies.
 
 
-Questions: 
+// Questions: 
 
-? Solutions Explorer ?
-? New Console Project ?
+// ? Solutions Explorer ?
+// ? New Console Project ?
+
+
+// RUN AS ADMIN
+// dotnet new console
+// dotnet restore (not necessary as of .NET 2.0?)
+// dotnet run
