c3efb023c7a5179e188c174c1f7376f144c33a01
Scott <burtonsb@gmail.com>
2018-08-20 11:33:18 -0400

    02

---
+++ notes.cs
@@ -0,0 +1,9 @@
+CLR - compiler so you can compile to different types of computers
+
+C vs .NET - .Net is a framework for writing windows applications
+
+"Namespace" - container for related classes
+Assembly - a container for related namespaces - DLL or EXE
+                                              - Dynamically Linked Library
+Application - container for multiple Assemblies.
+
