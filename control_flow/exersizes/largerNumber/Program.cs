08ecfc8a1f6982ef4557f893812d3597d8e6d7a7
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-08-22 20:02:03 +0000

    05 exersizes

---
+++ control_flow/exersizes/largerNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace largerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+            
+            var larger = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("The larger of the two: {0}", larger);
+            
+        }
+    }
+}
