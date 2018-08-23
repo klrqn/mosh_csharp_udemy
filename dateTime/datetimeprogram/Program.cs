af4b602e9f7b52fa3ef597d44dca14fd445726f6
john doe <scottbruceburton@gmail.com>
2018-08-23 19:39:27 -0400

    dateTime

---
+++ dateTime/datetimeprogram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace datetimeprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            
+            Console.WriteLine(now.Hour);
+            Console.WriteLine(now.Minute);
+            Console.WriteLine(now.Second);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            System.Console.WriteLine(now.ToLongDateString());
+            System.Console.WriteLine(now.toShortDateString());
+            System.Console.WriteLine(now.ToLongDateString()));
+            System.Console.WriteLine(now.ToShortTimeString());
+            System.Console.WriteLine(now.toString("yyyy-MM-dd"));
+
+            // google.
+            
+
+            
+        }
+    }
+}
