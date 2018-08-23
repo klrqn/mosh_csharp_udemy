8b7244cb649b06de0cc0d41cea4ff4a7d1979338
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-08-23 18:34:50 +0000

    sorted list 5 unique numbers

---
+++ arrays_lists/exersizes/fiveSortedNums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+﻿using System.Collections.Generic;
+
+namespace fiveSortedNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+        
+        
+            while (numbers.Count < 5)
+            {
+                Console.Write("Enter a unique number: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+                // Console.WriteLine("Number Written: {0}", number);
+                if (numbers.Contains(number))
+                    continue;
+                numbers.Add(number);
+            }
+            
+            numbers.Sort();
+            
+            foreach (var n in numbers)
+                Console.Write(" {0}, ", n);
+        }
+    }
+}
