7b820674539954ce367363b40fb60433f14c8c41
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-08-23 16:13:09 +0000

    lists ex1

---
+++ arrays_lists/exersizes/fb_likes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+﻿using System.Collections.Generic;
+
+namespace fb_likes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var friend = "";
+            var friendsList = new List<string>();
+            do
+            {
+                Console.Write("Enter a friend <enter> to finish: ");
+                friend = Console.ReadLine();
+                friendsList.Add(friend);
+            }
+            while (friend != "");
+            
+            var count = friendsList.Count - 1; // extra last empty 'friend'
+            // Console.WriteLine(count);
+            
+            if (count == 1)
+                Console.WriteLine("{0} likes your post", friendsList[0]);
+            if (count == 2)
+            {
+                Console.WriteLine("{0} and {1} like your post", 
+                friendsList[0], friendsList[1]);
+            }
+            if (count > 2)
+            {
+                Console.WriteLine("{0} and {1} and {2} others like your post", 
+                friendsList[0], friendsList[1], count-2);
+            }
+        }
+    }
+}
