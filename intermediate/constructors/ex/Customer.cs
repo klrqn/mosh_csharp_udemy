6a6836ca3d2cb785cb5743468c6f4c2d392194bd
john doe <scottbruceburton@gmail.com>
2018-08-28 19:17:34 -0400

    fixed constructor ex

---
+++ intermediate/constructors/ex/Customer.cs
@@ -0,0 +1,8 @@
+namespace constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
+}
---
+++ intermediate/constructors/ex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 123;
+            customer.Name = "scott";
+
+            System.Console.WriteLine(customer.Id);
+            System.Console.WriteLine(customer.Name);
+        }
+    }
+}
