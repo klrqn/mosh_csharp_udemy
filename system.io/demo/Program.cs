1d3b1b55f08f29b9a18fee4fada4d9859519c556
john doe <scottbruceburton@gmail.com>
2018-08-28 19:12:53 -0400

    switched intro intermediate dir

--- system.io/demo/Program.cs
+++ system.io/demo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace demo
 {
@@ -6,7 +6,28 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            
+            // FILE STATIC
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp.myfile.jpg", true);  // true it's allowed to override
+
+            if (File.Exists(path))
+            {
+                
+            }
+
+            var content = File.ReadAllText(path);
+
+            // FILEINFO
+
+            var fileinfo = @"c:\somefile.jpg";
+            fileinfo.CopyTo("...")
+            fileinfo.Delete();
+            if (fileinfo.Exists)
+            {
+                //
+            }
+
+
 
             Console.WriteLine("Hello World!");
         }
