149e991252cbcbf29e8504f7e63ea59c66bd75c2
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-08-29 20:38:19 +0000

    PascalCaseOutput

---
+++ intro/workingWithText/exercises/PascalCaseOutput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PascalCaseOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few words: ");
+            var input = Console.ReadLine();
+            
+            if (String.IsNullOrEmpty(input))
+                return;
+            
+            var pascalPhrase = "";
+            foreach (var word in input.Split(' '))
+            {
+                var pascalWord = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                pascalPhrase += pascalWord;
+            }
+            
+            Console.WriteLine("{0}", pascalPhrase);
+            
+        }
+    }
+}
