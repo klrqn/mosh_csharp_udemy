1da05731c76bf273b924c4eac06cb86d8dcb576a
john doe <scottbruceburton@gmail.com>
2018-08-29 22:07:57 -0400

    i feel dumb

---
+++ intermediate/constructors/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var me = new Person("Scott", 32, "Cranston");
+
+            // me.name = "Scott";
+            // me.age = 33;
+            // me.sex = 'M';
+            // me.location = "Cranston";
+
+            Console.WriteLine("Hi, my name is {0}, I am {1} and I live in {2}",
+                                me.age, me.sex, me.location);
+
+        }
+    }
+}
