e347666339f1d955a237c938a5e6294331f0125a
john doe <scottbruceburton@gmail.com>
2018-09-10 19:25:07 -0400

    section 3 inheritance and composition basics

---
+++ intermediate/class_associations/inheritance/PresentationObject.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            System.Console.WriteLine("Object copied to clipboard.");
+        }
+
+        public void Duplicate()
+        {
+            System.Console.WriteLine("Object was duplicated.");
+        }
+    }
+}
---
+++ intermediate/class_associations/inheritance/Text.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace inheritance
+{
+     public class Text : PresentationObject
+     {
+         public int FontSize { get; set; }
+         public string FontName { get; set; }
+
+         public void AddHyperlink(string url)
+         {
+             System.Console.WriteLine("We added a link to " + url);
+         }
+     }
+}
